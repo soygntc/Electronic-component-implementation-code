5a55cc5cdc34da857f33a884209aa12d3183cfaf
soygntc <giunator@espol.edu.ec>
2025-04-30 16:28:04 -0500

    Add files via upload

---
+++ AC-Solenoid-valve/AC-Solenoid-valve.ino
@@ -0,0 +1,13 @@
+/*************************************************
+* Componente: Valvula solenoide AC
+* Autora: Giulianna Torres Campodónico
+**************************************************/
+void setup() {
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
