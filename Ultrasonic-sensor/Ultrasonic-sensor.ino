eccdff52ff3449cbc38af5e90734858fb7752ee7
soygntc <giunator@espol.edu.ec>
2025-05-07 22:39:58 -0500

    Update Ultrasonic-sensor.ino

--- Ultrasonic-sensor/Ultrasonic-sensor.ino
+++ Ultrasonic-sensor/Ultrasonic-sensor.ino
@@ -2,12 +2,32 @@
  * Componente: Sensor ultrasónico HC-SR04
  * Autora: Giulianna Torres Campodónico
  ********************************************************/
+const int trigPin= 9;
+const int echoPin=10;
+float distance;
+long duration;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(trigPin, OUTPUT); // configures the trigger pin as an output
+  pinmode(echoPin, INPUT); // configures the echo pin as an input
+  Serial.begin(9600); // stars the serial communication
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH); // we send the pulse to prepare the sensor to send the 8 cycle burst
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration= pulseIn(echoPin, HIGH); // the echo pin is activated and stars to count the sound wave travel
+  distance= duration*0.0343/2; // calculating the distance 
+
+  Serial.print("La distancia es:")
+  Serial.print(distance); // prints the distance value in the serial monitor
+  delay(500); // 
+
+  
 
 }
