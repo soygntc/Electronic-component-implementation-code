361a7d7f5a023b49ff15c6872edc24c1b25286a7
soygntc <giunator@espol.edu.ec>
2025-05-07 22:40:16 -0500

    Update Ultrasonic-sensor.ino

--- Ultrasonic-sensor/Ultrasonic-sensor.ino
+++ Ultrasonic-sensor/Ultrasonic-sensor.ino
@@ -24,7 +24,7 @@ void loop() {
   duration= pulseIn(echoPin, HIGH); // the echo pin is activated and stars to count the sound wave travel
   distance= duration*0.0343/2; // calculating the distance 
 
-  Serial.print("La distancia es:")
+  Serial.print("La distancia es:");
   Serial.print(distance); // prints the distance value in the serial monitor
   delay(500); // 
 
