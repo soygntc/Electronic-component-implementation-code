3214947093c09b3aa543d102976ebbf512824808
soygntc <giunator@espol.edu.ec>
2025-05-07 23:34:28 -0500

    Update pH-sensor.ino

--- pH-sensor/pH-sensor.ino
+++ pH-sensor/pH-sensor.ino
@@ -2,12 +2,22 @@
  * Componente: Sensor pH, sonda y tarjeta PCB
  * Autora: Giulianna Torres Campodónico
  ********************************************************/
-void setup() {
-  // put your setup code here, to run once:
 
+const int analogPin = A0;
+float voltage;
+float pHValue;
+
+void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  voltage = analogRead(analogPin) * 5.0 / 1023.0; // converts ADC to voltage
+  pHValue = -5.7141*voltage + 22.295;        // value of pH using an calibration curve
 
+
+  Serial.print("El pH es: ");
+  Serial.print(pHValue);
+
+  delay(1000); // wait 1 second before the next reading
 }
