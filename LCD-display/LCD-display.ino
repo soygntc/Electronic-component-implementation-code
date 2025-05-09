7683140a65fd33a77934a9233e12b215ec4df80e
soygntc <giunator@espol.edu.ec>
2025-05-09 16:17:35 -0500

    Update LCD-display.ino

--- LCD-display/LCD-display.ino
+++ LCD-display/LCD-display.ino
@@ -2,12 +2,26 @@
  * Componente: Display LCD
  * Autora: Giulianna Torres Campodónico
  ********************************************************/
+#include <LiquidCrystal.h> // include the library code
+
+// associate any required LCD interface pins with the Arduino pin number to which they are connected
+int rs= 12, e= 11, d4= 5, d5= 4, d6= 3, d7= 2; 
+int number= 1;
+
+LiquidCrystal lcd(rs, e, d4, d5, d6, d7);
+
 void setup() {
-  // put your setup code here, to run once:
+  lcd.begin(20, 4); // LCDs number of rows and columns 
+  lcd.print("Hola mundo");
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  lcd.setCursor(0,1); // set the cursor to column 0, line 1 (the second row)
+  lcd.print(number);
+  number++;
+  delay(500);
+  
+  //lcd.clear()
 
 }
