97a60f2209a627e3dca05a8ef2b23025fe08cc98
soygntc <giunator@espol.edu.ec>
2025-05-09 18:23:29 -0500

    Update Flow-sensorDC.ino

--- Flow-sensorDC/Flow-sensorDC.ino
+++ Flow-sensorDC/Flow-sensorDC.ino
@@ -2,12 +2,37 @@
  * Componente: Sensor de flujo efecto hall DC
  * Autora: Giulianna Torres Campodónico
  ********************************************************/
-void setup() {
-  // put your setup code here, to run once:
 
+const pinSensor = 2; // this is the input pin on the Arduino
+volatile int pulses = 0; // its declared as volatile so that the ISR and the main program are used correctly
+unsigned long preTime = 0; / sensor pulses counter
+
+float caudal = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinSensor, INPUT); // sets the pin as an input
+  attachInterrupt(digitalPinToInterrupt(pinSensor), countingPulses, RISING);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (millis() - preTime >= 1000) { // verify and measures the flow every second
+    detachInterrupt(digitalPinToInterrupt(pinSensor)); // stop the Interrupt 
+    
+    caudal_Lmin = pulses / 11.0; // calculate the caudal with the factor convertion
 
+    Serial.print("Caudal: ");
+    Serial.print(caudal);
+    Serial.println(" L/min ");
+
+    // resets the counter, updates time, and reactivates the Interrupt.
+    pulses = 0;
+    preTime = millis();
+    attachInterrupt(digitalPinToInterrupt(pinSensor), countingPulses, RISING);
+  }
+}
+
+// ISR function
+void countingPulses() {
+  pulses++;
 }
