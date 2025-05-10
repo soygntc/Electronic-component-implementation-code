29b593d135e481638e19c6eb87c9f76eb33e9f2d
soygntc <giunator@espol.edu.ec>
2025-05-09 21:05:39 -0500

    Update Servo.ino

--- Servo/Servo.ino
+++ Servo/Servo.ino
@@ -2,12 +2,22 @@
  * Componente: Servomotor
  * Autora: Giulianna Torres Campodónico
  ********************************************************/
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h> // include the library
+Servo myservo;  // create servo object to control a servo
 
+int position = 0;    // variable to store the servo position in degrees
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  for (position= 0; position <= 180; position += 1) { // goes from 0 degrees to 180 degrees in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable "position"
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (position = 180; position >= 0; position -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(position);              // tell servo to go to position in variable "position"
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
 }
