d42210624b792d80ad3e530b13d5ab082ccf1289
soygntc <giunator@espol.edu.ec>
2025-05-09 21:11:53 -0500

    Update Pontenciometer.ino

--- Pontenciometer/Pontenciometer.ino
+++ Pontenciometer/Pontenciometer.ino
@@ -2,12 +2,18 @@
  * Componente: Potenciometro
  * Autora: Giulianna Torres Campodónico
  ********************************************************/
-void setup() {
-  // put your setup code here, to run once:
 
+// PARA LEER EL VALOR ANALOGICO
+
+int potPin = A0;  // pin where the potenciometer is connected
+int potValue = 0; // store the reading value
+
+void setup() {
+  Serial.begin(9600); // initialize the commnunication serial
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  potValue = analogRead(potPin);  // read the value (0-1023)
+  Serial.println(potValue);       // show the value 
+  delay(100);                     
 }
