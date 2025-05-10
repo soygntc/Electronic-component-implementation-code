7e70f122d0288508a631454b41cbe2807988385f
soygntc <giunator@espol.edu.ec>
2025-05-09 23:33:00 -0500

    Update H-bridge-module.ino

--- H-bridge-module/H-bridge-module.ino
+++ H-bridge-module/H-bridge-module.ino
@@ -2,12 +2,32 @@
  * Componente: Módulo puente H
  * Autora: Giulianna Torres Campodónico
  ********************************************************/
+int motorPin1= 13;
+int motorPin2= 12;
+int ena= 11; // PWM pin for speed control
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(ena,  OUTPUT); 
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // rotate motor forward 
+  digitalWrite(motorPin1,  HIGH);
+  digitalWrite(motorPin2, LOW);
+  analogWrite(ena, 127); // sets ~50% duty cycle
+  delay(2000);
 
+   // rotate motor backward 
+  digitalWrite(motorPin1,  LOW);
+  digitalWrite(motorPin2, HIGH);
+  analogWrite(ena, 127); 
+  delay(2000);
+
+  // stop motor
+  digitalWrite(motorPin1,  LOW);
+  digitalWrite(motorPin2, LOW);
+  delay(3000);
 }
